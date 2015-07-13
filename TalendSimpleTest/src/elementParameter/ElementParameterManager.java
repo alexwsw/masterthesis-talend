9560f8fd6f9532c375e109db36830a8e1363f3f3
Alex Becher <alexwsw@googlemail.com>
2015-07-13 18:04:33 +0200

    implemented sigleton pattern

--- TalendSimpleTest/src/elementParameter/ElementParameterManager.java
+++ TalendSimpleTest/src/elementParameter/ElementParameterManager.java
@@ -1,5 +1,21 @@
 package elementParameter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ElementParameterManager {
+	
+	private static ElementParameterManager EPManager = null;
+	private List<ElementParameter>parameters;
+	
+	private ElementParameterManager(){
+		parameters = new ArrayList<ElementParameter>();
+	}
+	
+	public static synchronized void getInstance() {
+		if(EPManager == null) {
+			EPManager = new ElementParameterManager(); 
+		}
+	}
 
 }
