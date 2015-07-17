1a78f0bf0921ba940baa171be487e8cad787e97f
Alex Becher <alexwsw@googlemail.com>
2015-07-17 17:19:38 +0200

    main interface modified

--- TalendSimpleTest/src/element/IElement.java
+++ TalendSimpleTest/src/element/IElement.java
@@ -1,5 +1,11 @@
 package element;
 
+import java.util.List;
+import elementParameter.IElementParameter;
+
 public interface IElement {
+	
+	public List <? extends IElementParameter> getElementParameters();
+	public void setElementParameter(List <? extends IElementParameter> parameters);
 
 }
