fd9642cdcea87efa1c48a22141c0d255a25b3031
Alex Becher <alexwsw@googlemail.com>
2015-09-11 18:26:35 +0200

    still a lot of work to do

--- WorkingOnTalendTree/src/dto/ColumnDTO.java
+++ WorkingOnTalendTree/src/dto/ColumnDTO.java
@@ -2,14 +2,115 @@ package dto;
 
 public class ColumnDTO {
 	
-	public boolean isKey;
-	public String length;
-	public String name;
-	public String nullable;
+	private boolean isKey;
+	private String length;
+	private String name;
+	private boolean nullable;
 	public String precision;
 	//value assigning witch case+enum, DTO's shouldn't contain any logic, 
 	//outsourcing to the next class could be better
-	public String sourceType;
-	public String type;
-	public boolean usefulColumn;		
+	private String sourceType;
+	private String type;
+	private boolean usefulColumn;
+	
+	
+	public ColumnDTO(boolean isKey, String length, String name,
+			boolean nullable, String precision, String sourceType, String type,
+			boolean usefulColumn) {
+		super();
+		this.isKey = isKey;
+		this.length = length;
+		this.name = name;
+		this.nullable = nullable;
+		this.precision = precision;
+		this.sourceType = sourceType;
+		this.type = type;
+		this.usefulColumn = usefulColumn;
+	}
+
+
+	public boolean isKey() {
+		return isKey;
+	}
+
+
+	public void setKey(boolean isKey) {
+		this.isKey = isKey;
+	}
+
+
+	public String getLength() {
+		return length;
+	}
+
+
+	public void setLength(String length) {
+		this.length = length;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public boolean isNullable() {
+		return nullable;
+	}
+
+
+	public void setNullable(boolean nullable) {
+		this.nullable = nullable;
+	}
+
+
+	public String getPrecision() {
+		return precision;
+	}
+
+
+	public void setPrecision(String precision) {
+		this.precision = precision;
+	}
+
+
+	public String getSourceType() {
+		return sourceType;
+	}
+
+
+	public void setSourceType(String sourceType) {
+		this.sourceType = sourceType;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public boolean isUsefulColumn() {
+		return usefulColumn;
+	}
+
+
+	public void setUsefulColumn(boolean usefulColumn) {
+		this.usefulColumn = usefulColumn;
+	}	
+	
+	
+	
+	
+	
+	
 }
--- WorkingOnTalendTree/src/dto/tMapDTO.java
+++ WorkingOnTalendTree/src/dto/tMapDTO.java
@@ -4,13 +4,69 @@ import java.util.Collection;
 
 public class tMapDTO {
 	
-	public String prefix;
+	private String prefix;
 	//need to get the proper data type for that
-	public Collection <? extends ColumnDTO> packageColumns;
-	public String matchColumn;
-	public String lookupTable;
-	public String lookupColumn;
-	public String packageOutputColumn_MatchColumn;
+	private Collection <? extends ColumnDTO> packageColumns;
+	private String lookupTable;
+	private String lookupColumn;
+	private String packageOutputColumn_MatchColumn;
+	
+	public tMapDTO(String prefix,
+			Collection<? extends ColumnDTO> packageColumns, String lookupTable,
+			String lookupColumn, String packageOutputColumn_MatchColumn) {
+		super();
+		this.prefix = prefix;
+		this.packageColumns = packageColumns;
+		this.lookupTable = lookupTable;
+		this.lookupColumn = lookupColumn;
+		this.packageOutputColumn_MatchColumn = packageOutputColumn_MatchColumn;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public Collection<? extends ColumnDTO> getPackageColumns() {
+		return packageColumns;
+	}
+
+	public void setPackageColumns(Collection<? extends ColumnDTO> packageColumns) {
+		this.packageColumns = packageColumns;
+	}
+
+	public String getLookupTable() {
+		return lookupTable;
+	}
+
+	public void setLookupTable(String lookupTable) {
+		this.lookupTable = lookupTable;
+	}
+
+	public String getLookupColumn() {
+		return lookupColumn;
+	}
+
+	public void setLookupColumn(String lookupColumn) {
+		this.lookupColumn = lookupColumn;
+	}
+
+	public String getPackageOutputColumn_MatchColumn() {
+		return packageOutputColumn_MatchColumn;
+	}
+
+	public void setPackageOutputColumn_MatchColumn(
+			String packageOutputColumn_MatchColumn) {
+		this.packageOutputColumn_MatchColumn = packageOutputColumn_MatchColumn;
+	}
+	
+	
+	
+	
+	
 	
 
 }
